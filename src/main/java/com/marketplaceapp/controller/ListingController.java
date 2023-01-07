1dcd5d892ad96cd9aa07f89b6b09ab12924a97cb
AlePouroullis <alexpouroullis123@gmail.com>
2023-01-08 00:42:12 +0100

    fixed typo in buildspec file name

--- src/main/java/com/marketplaceapp/controller/ListingController.java
+++ src/main/java/com/marketplaceapp/controller/ListingController.java
@@ -23,60 +23,60 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RestController
 @RequestMapping("/listings")
 public class ListingController {
-//
-//    private final ListingRepository repository;
-//    private final ListingModelAssembler assembler;
-//
-//    public ListingController(ListingRepository repository, ListingModelAssembler assembler) {
-//        this.repository = repository;
-//        this.assembler = assembler;
-//    }
-//
-//    @GetMapping("/{id")
-//    public EntityModel<Listing> one(@PathVariable Long id) {
-//        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
-//        return assembler.toModel(listing);
-//    }
-//
-//    @GetMapping("/")
-//    public CollectionModel<EntityModel<Listing>> all() {
-//        List<EntityModel<Listing>> listings = repository.findAll().stream().
-//                map(assembler::toModel).collect(Collectors.toList());
-//
-//        return CollectionModel.of(listings, linkTo(methodOn(ListingController.class).all()).withSelfRel());
-//    }
-//
-//    @PostMapping("/")
-//    ResponseEntity<EntityModel<Listing>> newListing(@RequestBody Listing listing) {
-//        listing.setStatus(ListingStatus.ACTIVE);
-//        Listing newListing = repository.save(listing);
-//        return ResponseEntity.created(linkTo(methodOn(ListingController.class).one(newListing.getId())).toUri())
-//                .body(assembler.toModel(newListing));
-//    }
-//
-//    @PatchMapping("/{id}/cancel")
-//    public ResponseEntity<?> cancel(@PathVariable long id) {
-//        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
-//        if (listing.getStatus() == ListingStatus.ACTIVE) {
-//            listing.setStatus(ListingStatus.CANCELLED);
-//            return ResponseEntity.ok(assembler.toModel(repository.save(listing)));
-//        }
-//        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE,
-//                MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE).body(Problem.create()
-//                .withTitle("Method not allowed")
-//                .withDetail("You can't cancel a listing that is in the " + listing.getStatus() + " status"));
-//    }
-//
-//    @PatchMapping("/{id}/complete")
-//    public ResponseEntity<?> complete(long id) {
-//        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
-//        if (listing.getStatus() == ListingStatus.ACTIVE) {
-//            listing.setStatus(ListingStatus.COMPLETED);
-//            return ResponseEntity.ok(assembler.toModel(repository.save(listing)));
-//        }
-//        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE,
-//                MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE).body(Problem.create()
-//                .withTitle("Method not allowed")
-//                .withDetail("You can't complete a listing that is in the " + listing.getStatus() + " status"));
-//    }
+
+    private final ListingRepository repository;
+    private final ListingModelAssembler assembler;
+
+    public ListingController(ListingRepository repository, ListingModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
+    }
+
+    @GetMapping("/{id}")
+    public EntityModel<Listing> one(@PathVariable Long id) {
+        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
+        return assembler.toModel(listing);
+    }
+
+    @GetMapping("/")
+    public CollectionModel<EntityModel<Listing>> all() {
+        List<EntityModel<Listing>> listings = repository.findAll().stream().
+                map(assembler::toModel).collect(Collectors.toList());
+
+        return CollectionModel.of(listings, linkTo(methodOn(ListingController.class).all()).withSelfRel());
+    }
+
+    @PostMapping("/")
+    ResponseEntity<EntityModel<Listing>> newListing(@RequestBody Listing listing) {
+        listing.setStatus(ListingStatus.ACTIVE);
+        Listing newListing = repository.save(listing);
+        return ResponseEntity.created(linkTo(methodOn(ListingController.class).one(newListing.getId())).toUri())
+                .body(assembler.toModel(newListing));
+    }
+
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<?> cancel(@PathVariable long id) {
+        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
+        if (listing.getStatus() == ListingStatus.ACTIVE) {
+            listing.setStatus(ListingStatus.CANCELLED);
+            return ResponseEntity.ok(assembler.toModel(repository.save(listing)));
+        }
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE,
+                MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE).body(Problem.create()
+                .withTitle("Method not allowed")
+                .withDetail("You can't cancel a listing that is in the " + listing.getStatus() + " status"));
+    }
+
+    @PatchMapping("/{id}/complete")
+    public ResponseEntity<?> complete(long id) {
+        Listing listing = repository.findById(id).orElseThrow(() -> new ListingNotFoundException(id));
+        if (listing.getStatus() == ListingStatus.ACTIVE) {
+            listing.setStatus(ListingStatus.COMPLETED);
+            return ResponseEntity.ok(assembler.toModel(repository.save(listing)));
+        }
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).header(HttpHeaders.CONTENT_TYPE,
+                MediaTypes.HTTP_PROBLEM_DETAILS_JSON_VALUE).body(Problem.create()
+                .withTitle("Method not allowed")
+                .withDetail("You can't complete a listing that is in the " + listing.getStatus() + " status"));
+    }
 }
--- src/main/java/com/marketplaceapp/view/listing/ListingModelAssembler.java
+++ src/main/java/com/marketplaceapp/view/listing/ListingModelAssembler.java
@@ -5,30 +5,26 @@ import com.marketplaceapp.model.listing.Listing;
 import com.marketplaceapp.model.listing.ListingStatus;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-public class ListingModelAssembler {
-    public ListingModelAssembler() {
-        int x = 0;
+@Component
+public class ListingModelAssembler implements RepresentationModelAssembler<Listing, EntityModel<Listing>> {
+    @Override
+    public EntityModel<Listing> toModel(Listing listing) {
+        EntityModel<Listing> listingModel = EntityModel.of(listing,
+                linkTo(methodOn(ListingController.class).one(listing.getId())).withSelfRel(),
+                linkTo(methodOn(ListingController.class).all()).withRel("listings"));
+
+        if (listing.getStatus() == ListingStatus.ACTIVE) {
+            listingModel.add(linkTo(methodOn(ListingController.class).
+                    cancel(listing.getId())).withRel("cancel"));
+            listingModel.add(linkTo(methodOn(ListingController.class).
+                    complete(listing.getId())).withRel("complete"));
+        }
+
+        return listingModel;
     }
 }
-
-//public class ListingModelAssembler implements RepresentationModelAssembler<Listing, EntityModel<Listing>> {
-//    @Override
-//    public EntityModel<Listing> toModel(Listing listing) {
-//        EntityModel<Listing> listingModel = EntityModel.of(listing,
-//                linkTo(methodOn(ListingController.class).one(listing.getId())).withSelfRel(),
-//                linkTo(methodOn(ListingController.class).all()).withRel("listings"));
-//
-//        if (listing.getStatus() == ListingStatus.ACTIVE) {
-//            listingModel.add(linkTo(methodOn(ListingController.class).
-//                    cancel(listing.getId())).withRel("cancel"));
-//            listingModel.add(linkTo(methodOn(ListingController.class).
-//                    complete(listing.getId())).withRel("complete"));
-//        }
-//
-//        return listingModel;
-//    }
-//}
